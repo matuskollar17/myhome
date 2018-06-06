1a979b0339613bb4d0911abdc01d5b61c0f945fc
Matus Kollar <matuskollar17@gmail.com>
2018-06-06 15:54:23 +0200

    style building, login,logout

--- src/main/java/sk/mk/myhome/controller/MyHomeController.java
+++ src/main/java/sk/mk/myhome/controller/MyHomeController.java
@@ -12,5 +12,40 @@ public class MyHomeController {
 		//fillModel(model);
 		return "index";
 	}
+	
+	@RequestMapping("/nav")
+	public String nav(Model model) {
+		//fillModel(model);
+		return "nav";
+	}
+	
+	@RequestMapping("/profile")
+	public String profile(Model model) {
+		//fillModel(model);
+		return "profile";
+	}
 
+	@RequestMapping("/admini")
+	public String admin(Model model) {
+		//fillModel(model);
+		return "admin";
+	}
+	
+	@RequestMapping("/home")
+	public String home(Model model) {
+		//fillModel(model);
+		return "home";
+	}
+	
+	@RequestMapping("/garage")
+	public String garage(Model model) {
+		//fillModel(model);
+		return "garage";
+	}
+	
+	@RequestMapping("/garden")
+	public String garden(Model model) {
+		//fillModel(model);
+		return "garden";
+	}
 }
